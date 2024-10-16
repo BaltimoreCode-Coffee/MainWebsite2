ee7d1ce55cce931d5863d15fc83467cb007f3ca2
Briceson <royzone4@gmail.com>
2024-10-16 15:10:36 -0400

    added footer back along with social links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,8 @@
 import "./Footer.css";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faFacebook,
-  faGithub,
+  faMeetup,
   faLinkedin,
   faSlack
 } from "@fortawesome/free-brands-svg-icons";
@@ -14,7 +13,20 @@ function Footer() {
       <div className="footer-logo">
         <span className="email"><a href="">bmorecodecoffee@gmail.com</a></span>
       </div>
-      <div className="menus">
+      <div className="footer-links">
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/events">Events</Link>
+          </li>
+          <li>
+            <Link to="/resources">Volunteer</Link>
+          </li>
+          </ul>
+      </div>
+      {/* <div className="menus">
       <div className="mini-menu" id="events">
         <h4>Event</h4>
         <ul>
@@ -35,28 +47,24 @@ function Footer() {
                 <li>FAQs</li>
                 <li>Join us</li>
             </ul>
-        </div>
-      </div>
+        </div> 
+      </div> */}
       
         
       <div className="bottom-half"> 
-        <p>© 2024 Baltimore Code and Coffee All rights reserved.</p>
+        <p className="copyright">© 2024 Baltimore Code and Coffee All rights reserved.</p>
         <ul className="social-list">
           <li>
             {" "}
-            <FontAwesomeIcon icon={faGithub} />
+            <a href="https://www.linkedin.com/company/baltimore-code-coffee/"><FontAwesomeIcon className="social-icon" icon={faLinkedin} /></a>
           </li>
           <li>
             {" "}
-            <FontAwesomeIcon icon={faFacebook} />
-          </li>
+            <a href="https://baltimoretech.slack.com/archives/CTDNSM6EP"><FontAwesomeIcon className="social-icon" icon={faSlack} /></a>
+          </li>  
           <li>
             {" "}
-            <FontAwesomeIcon icon={faLinkedin} />
-          </li>
-          <li>
-            {" "}
-            <FontAwesomeIcon icon={faSlack} />
+            <a href="https://www.meetup.com/baltimore-code-and-coffee"><FontAwesomeIcon className="social-icon" icon={faMeetup} /></a>
           </li>
         </ul>
       </div>
