a02c693694ad941e998301e4ae6ac0544df5ab41
Briceson <royzone4@gmail.com>
2024-11-19 16:45:25 -0500

    Adjusted the sizes to be more in line with figma

--- src/components/BlogView/BlogView.jsx
+++ src/components/BlogView/BlogView.jsx
@@ -7,9 +7,7 @@ export default function BlogView() {
     return (
         <div className="blog-view-wrapper">
             <div className="blog-view">
-                <div>
-                    <h1><b>Our Blog</b></h1>
-                </div>
+                <h2><b>Our Blog</b></h2>
                 <div className="blog-view-desc">
                     <h3>Your Daily Brew of Tech Insights, Community, Stories and Coding Tips</h3>
                 </div>
@@ -31,7 +29,7 @@ export default function BlogView() {
                     </div>
                 </div>
                 <div className="blog-view-footer">
-                    <a href="/blog" className="blog-view-footer-link">View All Posts</a>
+                    <a href="/blog" className="blog-view-footer-link">See All Articles</a>
                 </div>
             </div>
         </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 export default function Card({ title, tag, desc }) {
-    return (
-        <div className="card">  
+  return (
+    <div className="card">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Placeholder"
+        className="card-image"
+      />
+      <div className="card-info">
         <p className="card-tag">{tag}</p>
-        <h2 className="card-title">{title}</h2>
+        <h3 className="card-title">{title}</h3>
         <p className="card-desc">{desc}</p>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
