53dcf0e75bde3587978a71165db056ea0ec2203b
valentineokundaye <valentineokundaye21@gmail.com>
2025-01-16 16:55:28 -0500

    changed h2 tag to p tag

--- src/components/Volunteer/Volunteer.jsx
+++ src/components/Volunteer/Volunteer.jsx
@@ -8,18 +8,18 @@ export default function Volunteer() {
   
     <h1 className="volunteer">Make Your Mark</h1>
     <div className="volunteer_top">
-      <h2 className="volunteer">Play an active role in Baltimore’s rise as a tech hub</h2>
-      <h2 className="volunteer">by joining our volunteer team!</h2>
+      <p>Play an active role in Baltimore’s rise as a tech hub by joining our volunteer team!</p>
+   
     </div>
 
     <img src={collage} className="volunteer collage"/>
 
     <div className="volunteer_bottom">
-      <h2 className="">If you’re passionate about coding and looking to increase your involvement in the 
+      <p>If you’re passionate about coding and looking to increase your involvement in the 
         tech community, becoming a volunteer at Code Coffee might be for you. As a volunteer, 
         you’ll have the opportunity to turn your technical skills into leadership skills, and 
         contribute to maintaining a vibrant tech ecosystem. Join us today and be a part of 
-        something meaningful!</h2>
+        something meaningful!</p>
     </div>
 
     <button className="volunteer button">Volunteer</button>
