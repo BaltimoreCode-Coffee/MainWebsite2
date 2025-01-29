e75b3a9d3278e48dc4f63689cb2fdb2d221a067d
Briceson <royzone4@gmail.com>
2025-01-29 13:06:59 -0500

    Minor adjustments for the blog card and blog Hero

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -5,17 +5,20 @@ import Stack from "@mui/material/Stack";
 import Pagination from "@mui/material/Pagination";
 export default function Blog() {
   return (
-    <div className="blog">
-      <section className="blog-header">
+    <section className="blog">
+      <div className="blog-header">
         <h1>Our Blogs</h1>
         <h4>Daily Brew of Tech Insights, Community Stories and Coding Tips</h4>
-      </section>
-
-      <FullBlogList />
-
+      </div>
+    
+        <FullBlogList />
+      <br />
+      <br />
+      <br />
+      
       <Stack spacing={2}>
         <Pagination count={10} color="secondary" />
       </Stack>
-    </div>
+    </section>
   );
 }
